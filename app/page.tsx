e97be26b4807ad13ec4a630a5cae48a3a08595fd
Ashish Makwana <ashishn.makwana@gmail.com>
2025-01-06 19:27:48 +0530

    new pages and ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="alignment mt-10 md:mt-20">
       <h1>NextJs E-commerce Store Project</h1>
     </div>
   );
---
+++ app/reviews/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function ReviewsPage() {
+  return <div className="alignment mt-10 md:mt-20">
+    <h1>Reviews</h1>
+    <Button type="button" >Click me</Button>
+  </div>;
+}
+
+export default ReviewsPage;
