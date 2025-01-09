c537cd8d64733f7694d18ab12152fe260247b4bd
Ashish Makwana <ashishn.makwana@gmail.com>
2025-01-09 14:44:22 +0530

    hero section complete

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 function HeroSection() {
   return (
-    <section className='grid lg:grid-cols-2 gap-20 pb-16'>
+    <section className='grid md:grid-cols-2 gap-8 items-center pb-16'>
       <div className='order-1 flex flex-col gap-y-4 md:gap-y-6'>
-        <h1 className='text-2xl max-w-2xl md:text-4xl font-bold tracking-wide '>
+        <h1 className='text-2xl max-w-2xl md:text-3xl lg:text-4xl font-bold tracking-wide '>
           NextJS E-commerce Store Project
         </h1>
         <p className='text-md max-w-xl md:text-lg font-normal leading-7 text-muted-foreground'>
@@ -22,7 +23,7 @@ function HeroSection() {
         </Button>
       </div>
       <div className='md:order-2'>
-        <h2>Image placeholder</h2>
+        <Image src='https://npwwuqihxoqrnuhuqtje.supabase.co/storage/v1/object/public/main-bucket/1723649174470-pexels-designecologist-1005058.jpg' width={4000} height={4000} quality={90} alt="hero-image" className="rounded w-full h-full object-cover" />
       </div>
     </section>
   );
